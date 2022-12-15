5a351c7ea846d54494de825b9972036fef239ee5
lcj <lcj@kth.se>
2022-12-15 18:12:20 +0100

    Initial Commit

---
+++ test3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Leandro Carocca Jeria on 2022-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test3/test3App.swift
@@ -0,0 +1,17 @@
+//
+//  test3App.swift
+//  test3
+//
+//  Created by Leandro Carocca Jeria on 2022-12-15.
+//
+
+import SwiftUI
+
+@main
+struct test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
